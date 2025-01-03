48acff0a898bf06533a4e40e25b85cddeb835ed3
musya <dudumikah@gmail.com>
2025-01-03 22:50:35 +0500

    feat: Add i18n

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,98 @@
+import {Nunito} from "next/font/google";
+import {NextIntlClientProvider} from "next-intl";
+import {getMessages} from "next-intl/server";
+import {unstable_setRequestLocale} from "next-intl/server";
+
+//styles
+import "./globals.css";
+import React from "react";
+
+// Подключение Google Fonts (Raleway)
+const nunito = Nunito({
+    subsets: ["latin", "cyrillic"],
+    weight: ["300", "400", "600", "700"], // Choose the weights you need
+    variable: "--font-nunito",
+});
+
+interface LayoutProps {
+    children: React.ReactNode;
+    params: Promise<{ locale?: string }>; // Определяем params как промис
+}
+
+export default async function RootLayout({children, params}: LayoutProps) {
+    // Устанавливаем локаль для запроса
+
+    // SEO и OG данные для узбекского и русского языков
+    const siteUrl = "https://oqid.uz";
+
+    type Locales = "ru" | "uz";
+
+    const seoData: Record<
+        Locales,
+        {
+            title: string;
+            description: string;
+            imageUrl: string;
+            canonicalUrl: string;
+        }
+    > = {
+        ru: {
+            title: "Oq Id | Ташкент",
+            description: "Единая система регистрации",
+            imageUrl: `${siteUrl}/favicon.ico`,
+            canonicalUrl: `${siteUrl}/ru`,
+        },
+        uz: {
+            title: "Oq Id | Toshkent",
+            description: "Yagona ro‘yxatga olish tizimi",
+            imageUrl: `${siteUrl}/favicon.ico`,
+            canonicalUrl: `${siteUrl}/uz`,
+        },
+    };
+
+    const resolvedParams = await params;
+
+    const locale = resolvedParams?.locale === "uz" ? "uz" : "ru";
+
+    const {title, description, imageUrl, canonicalUrl} = seoData[locale];
+    unstable_setRequestLocale(locale);
+
+    const messages = await getMessages({locale});
+
+    return (
+        <html lang={locale}>
+        <head>
+            {/* Базовые SEO-теги */}
+            <meta name="description" content={description}/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <meta charSet="UTF-8"/>
+            <meta name="robots" content="index, follow"/>
+            <meta name="author" content="Sammy"/>
+            <link rel="canonical" href={canonicalUrl}/>
+            <link rel="icon" href="/favicon.ico"/>
+            {/* Open Graph (OG) теги */}
+            <meta property="og:title" content={title}/>
+            <meta property="og:description" content={description}/>
+            <meta property="og:type" content="website"/>
+            <meta property="og:url" content={canonicalUrl}/>
+            <meta property="og:image" content={imageUrl}/>
+            <meta property="og:image:width" content="1200"/>
+            {/* Рекомендованные размеры */}
+            <meta property="og:image:height" content="630"/>
+            <meta property="og:site_name" content="Oq Id"/>
+            {/* Теги для Twitter */}
+            <meta name="twitter:card" content="summary_large_image"/>
+            <meta name="twitter:title" content={title}/>
+            <meta name="twitter:description" content={description}/>
+            <meta name="twitter:image" content={imageUrl}/>
+            <title>{title}</title>
+        </head>
+        {/*antialiased content-hidden*/}
+        <body className={`${nunito.variable}`}>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+            {children}
+        </NextIntlClientProvider>
+        </body>
+        </html>
+    );
+}
