236bcd69217659a380ca04565bd56e088e1ed077
Abdukahkim <dudumikah@gmail.com>
2025-01-04 14:05:15 +0500

    feat: Middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,23 @@
+import { NextResponse, NextRequest } from 'next/server'; // Импортируем NextResponse
 import createMiddleware from 'next-intl/middleware';
 import { routing } from './i18n/routing';
 
-export default createMiddleware(routing);
+// Основная логика middleware
+export default async function middleware(req: NextRequest) {
+    const url = req.nextUrl;
+    const locale = req.cookies.get('locale') || 'ru'; // Проверяем куки, используем 'ru' по умолчанию
 
+    // Если пользователь на корневом пути, перенаправляем его на /ru или /uz
+    if (url.pathname === '/') {
+        url.pathname = `/${locale}`;
+        return NextResponse.redirect(url);
+    }
+
+    // Применяем стандартную обработку через next-intl middleware
+    return createMiddleware(routing)(req);
+}
+
+// Конфигурация matcher
 export const config = {
-    matcher: ['/', '/(ru|uz)/:path*'],
+    matcher: ['/', '/(ru|uz)/:path*'], // Перехватываем корень и все пути с локалями
 };
