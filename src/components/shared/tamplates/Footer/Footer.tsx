25ec5c634441b0cf91f93489dc612c8ee66c7540
Abdukahkim <dudumikah@gmail.com>
2025-01-30 11:01:00 +0500

    feat: Footer

--- src/components/shared/tamplates/Footer/Footer.tsx
+++ src/components/shared/tamplates/Footer/Footer.tsx
@@ -3,19 +3,22 @@ import {cn} from '@lib/utils';
 import Image from "next/image";
 import {FooterProvider} from './FooterProvider';
 import {useTranslations} from "next-intl";
+import Link from "next/link";
 
 interface Props {
     className?: string;
 }
-
+f
 export const Footer = ({className}: Props) => {
     const t = useTranslations("Footer");
     return (
         <FooterProvider>
             <div className={cn(className, "grid grid-cols-4 gap-8")}>
                 <div className={"flex flex-col gap-6"}>
-                    <Image src={"/svg/footer/logo.svg"} alt={"Footer logo of Masterium"} width={500}
-                           height={100} className={"w-full max-w-[350px] h-auto"} quality={100}/>
+                    <Link href="/">
+                        <Image src={"/svg/footer/logo.svg"} alt={"Footer logo of Masterium"} width={500}
+                               height={100} className={"w-full max-w-[350px] h-auto"} quality={100}/>
+                    </Link>
                     <h3 className={"text-maket-gold"}>{t("logo.title")}</h3>
                 </div>
                 <div className={"flex justify-center"}>
@@ -41,8 +44,10 @@ export const Footer = ({className}: Props) => {
                             {t('download.title')}
                         </h4>
                         <div className={"flex flex-col gap-4"}>
-                                <Image src={"/svg/footer/Apple.svg"} alt={"Download from Apple Store Icon"} width={200} height={100} quality={100} className={"h-10 w-auto"} />
-                            <Image src={"/svg/footer/GooglePlay.svg"} alt={"Download from Apple Store Icon"} width={200} height={100} quality={100} className={"h-10 w-auto"} />
+                            <Image src={"/svg/footer/Apple.svg"} alt={"Download from Apple Store Icon"} width={200}
+                                   height={100} quality={100} className={"h-10 w-auto"}/>
+                            <Image src={"/svg/footer/GooglePlay.svg"} alt={"Download from Apple Store Icon"} width={200}
+                                   height={100} quality={100} className={"h-10 w-auto"}/>
                         </div>
                     </div>
                 </div>
