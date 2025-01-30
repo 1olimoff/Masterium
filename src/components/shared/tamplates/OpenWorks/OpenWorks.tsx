7bffcbd62c026dfb91808acc1807e246a38ebb66
Abdukahkim <dudumikah@gmail.com>
2025-01-30 21:47:19 +0500

    feat: OpenWorks Title component

--- src/components/shared/tamplates/OpenWorks/OpenWorks.tsx
+++ src/components/shared/tamplates/OpenWorks/OpenWorks.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { cn } from '@lib/utils';
 import {AdGrid} from "@/components/shared/elements/advertising/AdGrid";
+import {Title} from "./Title/Title"
 
 interface Props {
     className?: string;
@@ -10,6 +11,7 @@ export const OpenWorks = ({ className }: Props) => {
     return (
         <div className={cn(className, "w-full flex flex-col gap-12 py-6")}>
             <AdGrid />
+            <Title />
         </div>
     );
 };
