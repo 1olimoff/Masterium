0edf440d5ed98f7a67e08e6fad9994a37c30a3e0
Abdukahkim <dudumikah@gmail.com>
2025-01-31 15:17:33 +0500

    feat

--- src/components/shared/tamplates/OpenWorks/Filter/FilterBar.tsx
+++ src/components/shared/tamplates/OpenWorks/Filter/FilterBar.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { cn } from '@lib/utils';
+import {useTranslations} from "next-intl";
+import {Input} from "@/components/ui/input";
 
 interface Props {
     className?: string;
 }
 
 export const FilterBar = ({ className }: Props) => {
+    const t = useTranslations();
     return (
-        <div className={cn(className)}></div>
+        <div className={cn(className, "w-full flex justify-between gap-4")}>
+            <div className={"w-[27%] flex flex-col gap-4"}>
+    <p>{t('OpenWorks.filter.catalog.title')}</p>
+                <Input />
+            </div>
+        </div>
     );
 };
