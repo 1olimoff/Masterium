4c5499c21ed0e4299aac95897dc80fbed8b58e10
Abdukahkim <dudumikah@gmail.com>
2025-02-09 19:54:52 +0500

    feat: xz

--- src/components/shared/tamplates/Services/Service/ServicePerson/Content/InfoTab/InfoTab.tsx
+++ src/components/shared/tamplates/Services/Service/ServicePerson/Content/InfoTab/InfoTab.tsx
@@ -10,10 +10,13 @@ interface Props {
 }
 
 export const InfoTab = ({ className }: Props) => {
+    const t = useTranslations();
     return (
         <div className={cn(className, "w-full h-full py-8 flex flex-col gap-8")}>
             <div className={"w-full px-8 flex flex-col gap-8 rounded-xl bg-white"}>
-                <h2></h2>
+                <h2>
+                    {t('Services.Service.ServicePerson.infoTab.infoTitle')}
+                </h2>
             </div>
         </div>
     );
