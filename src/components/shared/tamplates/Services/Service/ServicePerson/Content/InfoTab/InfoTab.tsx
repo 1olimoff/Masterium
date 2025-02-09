0bb8968b50baa72eeada82a47b5655ef5ae42bfc
Abdukahkim <dudumikah@gmail.com>
2025-02-09 19:50:06 +0500

    feat: xz

--- src/components/shared/tamplates/Services/Service/ServicePerson/Content/InfoTab/InfoTab.tsx
+++ src/components/shared/tamplates/Services/Service/ServicePerson/Content/InfoTab/InfoTab.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { cn } from '@lib/utils';
+import {useTranslations} from "next-intl";
 
 interface Props {
     infoText: string;
     experienceText?: string;
+    tags: string[];
     className?: string;
 }
 
 export const InfoTab = ({ className }: Props) => {
     return (
-        <div className={cn(className)}>
-
+        <div className={cn(className, "w-full h-full py-8 flex flex-col gap-8")}>
+            <div className={"w-full px-8 flex flex-col gap-8 rounded-xl bg-white"}>
+                <h2></h2>
+            </div>
         </div>
     );
 };
