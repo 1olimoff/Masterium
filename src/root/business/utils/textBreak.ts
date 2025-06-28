d7ef58f8ca79ad072500554ce885c1aa416b67f3
iMac <imac@MacBook-Air-iMac.local>
2025-06-28 12:09:11 -0700

    edit textbreak.ts

--- src/root/business/utils/textBreak.ts
+++ src/root/business/utils/textBreak.ts
@@ -1,16 +1,16 @@
-// import React from 'react'
+// // import React from 'react'
 
-// export const formatTextWithLineBreaks = (text: string): JSX.Element[] => {
-<<<<<<< HEAD
-//     return text.split('\n').map((line, index) => (
-//         <React.Fragment key = {index} >
-//             {line}
-//             < br / >
-=======
-//     return( text.split('\n').map((line, index) => (
-//         <React.Fragment key = {index} >
-//             {line}
->>>>>>> Fix layout params error
-//             </React.Fragment>
-//     ));
-// };
+// // export const formatTextWithLineBreaks = (text: string): JSX.Element[] => {
+// <<<<<<< HEAD
+// //     return text.split('\n').map((line, index) => (
+// //         <React.Fragment key = {index} >
+// //             {line}
+// //             < br / >
+// =======
+// //     return( text.split('\n').map((line, index) => (
+// //         <React.Fragment key = {index} >
+// //             {line}
+// >>>>>>> Fix layout params error
+// //             </React.Fragment>
+// //     ));
+// // };
