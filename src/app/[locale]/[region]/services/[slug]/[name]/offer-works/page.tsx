5ec32fd311f49b9f4e15a728e65eedbe7a03e4d0
iMac <imac@MacBook-Air-iMac.local>
2025-07-30 15:54:26 -0700

    yandex metrika qoshildi

--- src/app/[locale]/[region]/services/[slug]/[name]/offer-works/page.tsx
+++ src/app/[locale]/[region]/services/[slug]/[name]/offer-works/page.tsx
@@ -1,15 +1,24 @@
 import OfferWork from '@/root/ui/dev/components/shared/tamplates/OfferWork/OfferWork';
 import React from 'react';
+import { NextPage } from 'next';
 
-async function WorkOffers({ params }: { params: { slug: string; name: string } }) {
-  const slug = params.slug;
-  const user_uuid = params.name;
+interface PageProps {
+  params: Promise<{
+    locale: string;
+    region: string;
+    slug: string;
+    name: string;
+  }>;
+}
+
+const WorkOffers: NextPage<PageProps> = async ({ params }) => {
+  const { slug, name: user_uuid } = await params;
 
   return (
     <div>
       <OfferWork slug={slug} userUuid={user_uuid} />
     </div>
   );
-}
+};
 
 export default WorkOffers;
